42f852d173f5d4bef92d38f57beac097200ae602
Opraveen <pparihar522@gmail.com>
2024-05-07 12:31:11 +0530

    Add files via upload

---
+++ netflix_tranformed.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, upper
+
+def transform_csv_to_uppercase(input_file_path, output_file_path):
+    # Initialize Spark session
+    spark = SparkSession.builder \
+        .appName("CSV to Uppercase") \
+        .getOrCreate()
+
+    # Read CSV file into DataFrame
+    df = spark.read.option("header", True).csv(input_file_path)
+
+    # Apply transformation: Convert "description" column to uppercase
+    df_transformed = df.withColumn("description", upper(col("description")))
+
+    # Write transformed DataFrame to output location
+    df_transformed.write.mode("overwrite").csv(output_file_path)
+
+    # Stop Spark session
+    spark.stop()
+
+# Example usage of the function
+if __name__ == "__main__":
+    input_path = "/home/opraveen/airflow_workspace/airflow/netflix_titles.csv"
+    output_path = "home/opraveen/airflow_workspace/airflow/netflix_output.csv"
+    transform_csv_to_uppercase(input_path, output_path)
+
